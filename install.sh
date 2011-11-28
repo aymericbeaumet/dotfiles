c3ab9de1c8c91801bb9b185e2f13fea25078609f
Aymeric Beaumet <aymeric@beaumet.fr>
2011-11-28 23:49:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-ln -sf `pwd`/git/gitconfig	~/.gitconfig
-ln -sf `pwd`/vim/vim		~/.vim
-ln -sf `pwd`/vim/vimrc		~/.vimrc
-ln -sf `pwd`/zsh/zshrc		~/.zshrc
+ln -sf `pwd`/git/gitconfig ~/.gitconfig
+ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
+rm -f ~/.vim && ln -sf `pwd`/vim/vim/ ~/.vim
+ln -sf `pwd`/vim/vimrc ~/.vimrc
+ln -sf `pwd`/zsh/zshrc ~/.zshrc
