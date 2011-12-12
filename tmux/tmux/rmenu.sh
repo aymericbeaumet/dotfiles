a4d054b02525fcb084b4fdb70f41e8ca3fc1038f
Aymeric Beaumet <aymeric@beaumet.fr>
2011-12-12 13:47:07 +0100

    Fix bug

--- tmux/tmux/rmenu.sh
+++ tmux/tmux/rmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-if echo `uptime` | grep day ; then
+if uptime | grep day > /dev/null 2> /dev/null ; then
   uptime_day=$(uptime | sed 's#.\+ up \([0-9]\+\) day.\+#\1#')
 else
   uptime_day=0
