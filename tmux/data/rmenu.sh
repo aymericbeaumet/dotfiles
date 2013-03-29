08d8212e125fdb4b50fcf9fe730c5c20b5338a38
Aymeric Beaumet <aymeric@beaumet.fr>
2013-03-29 18:06:53 +0100

    Update tmux configuration file

--- tmux/data/rmenu.sh
+++ tmux/data/rmenu.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+ip="$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
+
 uptime_day=0
 if uptime | grep day >/dev/null 2>&1 ; then
   uptime_day=$(uptime | sed 's#.\+ up \([0-9]\+\) day.\+#\1#')
 fi
 
-echo "[$uptime_day] [$(date '+%d.%m.%Y %H:%M')]"
+if [ "$(uname -s)" = 'Darwin' ] ; then
+  OS='MacOS X'
+elif [ "$(uname -s)" = 'Linux' ] ; then
+  OS="$(uname -o)"
+else
+  OS='Unkown OS'
+fi
+
+echo "[$(whoami)@$(hostname)] [$ip] [$OS] [${uptime_day}d]"
 
 exit 0
