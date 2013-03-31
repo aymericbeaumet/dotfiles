55b33a7ea452fc6ebf5fac7c02442e922d960347
Aymeric Beaumet <aymeric@beaumet.fr>
2013-03-31 22:33:23 +0200

    Change tmux right menu

--- tmux/data/rmenu.sh
+++ tmux/data/rmenu.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
-ip="$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
-
-uptime_day=0
-if uptime | grep day >/dev/null 2>&1 ; then
-  uptime_day=$(uptime | sed 's#.\+ up \([0-9]\+\) day.\+#\1#')
+user='unknown_user'
+if which 'whoami' >/dev/null 2>&1 ; then
+  user="$(whoami)"
 fi
 
-if [ "$(uname -s)" = 'Darwin' ] ; then
-  OS='MacOS X'
-elif [ "$(uname -s)" = 'Linux' ] ; then
-  OS="$(uname -o)"
-else
-  OS='Unkown OS'
+host='unknown_host'
+if which 'host' >/dev/null 2>&1 ; then
+  host="$(hostname)"
 fi
 
-echo "[$(whoami)@$(hostname)] [$ip] [$OS] [${uptime_day}d]"
+ipv4='unknown_ip'
+if which 'ip' >/dev/null 2>&1 ; then
+  ipv4="$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
+fi
+
+uptime_day='unknown_uptime'
+if which 'uptime' >/dev/null 2>&1 && uptime | grep day >/dev/null 2>&1 ; then
+  uptime_day=$(uptime | sed 's#.\+ up \([0-9]\+ days*\).\+#\1#')
+fi
+
+os='unknown_OS'
+arch='unknown_arch'
+if which 'uname' >/dev/null 2>&1 ; then
+  if [ "$(uname -s)" = 'Linux' ] ; then
+    os="$(uname -o)"
+  elif [ "$(uname -s)" = 'Darwin' ] ; then
+    os='Mac OS'
+  fi
+
+  arch="$(uname -m)"
+fi
+
+echo "[$user@$host] [$ipv4] [$os $arch] [$uptime_day]"
 
 exit 0
