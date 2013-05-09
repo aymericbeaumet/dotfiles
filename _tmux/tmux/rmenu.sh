2c156a93666bc173fb7fd92b50a340d5cf7dc895
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-09 19:51:18 +0200

    Some fix in zshrc

--- _tmux/tmux/rmenu.sh
+++ _tmux/tmux/rmenu.sh
@@ -1,37 +1,21 @@
 #!/bin/sh
 
-user='unknown_user'
-if which 'whoami' >/dev/null 2>&1 ; then
-  user="$(whoami)"
-fi
-
-host='unknown_host'
-if which 'hostname' >/dev/null 2>&1 ; then
-  host="$(hostname)"
-fi
-
-ipv4='unknown_ip'
-if which 'ip' >/dev/null 2>&1 ; then
-  ipv4="$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
-fi
-
-uptime_day='unknown_uptime'
-if which 'uptime' >/dev/null 2>&1 && uptime | grep day >/dev/null 2>&1 ; then
-  uptime_day=$(uptime | sed 's#.\+ up \([0-9]\+ days*\).\+#\1#')
-fi
-
 os='unknown_OS'
 arch='unknown_arch'
 if which 'uname' >/dev/null 2>&1 ; then
+  os="$(uname -s)"
   if [ "$(uname -s)" = 'Linux' ] ; then
     os="$(uname -o)"
-  elif [ "$(uname -s)" = 'Darwin' ] ; then
-    os='Mac OS'
   fi
 
   arch="$(uname -m)"
 fi
 
-echo "[$user@$host] [$ipv4] [$os $arch] [$uptime_day]"
+uptime_day='unknown_uptime'
+if which 'uptime' >/dev/null 2>&1 && uptime | grep day >/dev/null 2>&1 ; then
+  uptime_day=$(uptime | awk '{print $3;}')
+fi
+
+echo "[$os $arch] [$uptime_day]"
 
 exit 0
