d9b2b813f519aa5efb5032e4300e77855f7329b8
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-09 13:55:56 +0200

    Split in more smaller configuration files

---
+++ _zsh/zsh/git.zsh
@@ -0,0 +1,18 @@
+#############
+# Git stuff #
+#############
+
+alias ga='git add'
+alias gA='git add -A'
+alias gcamend='git commit --amend'
+
+# based on ~/.gitconfig
+alias gbr='git branch'
+alias gci='git commit -m'
+alias gco='git checkout'
+alias gdf='git diff'
+alias gps='git push'
+alias gpl='git pull'
+alias grs='git reset'
+alias gst='git status'
+alias gsh='git stash'
