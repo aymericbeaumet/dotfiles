74993b112c9d1f280281e17e3edcfae7b32eefb8
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-09 13:59:29 +0200

    Update git aliases in zsh

--- _zsh/zsh/git.zsh
+++ _zsh/zsh/git.zsh
@@ -4,15 +4,15 @@
 
 alias ga='git add'
 alias gA='git add -A'
-alias gcamend='git commit --amend'
+alias gamend='git commit --amend'
 
 # based on ~/.gitconfig
 alias gbr='git branch'
 alias gci='git commit -m'
 alias gco='git checkout'
 alias gdf='git diff'
-alias gps='git push'
-alias gpl='git pull'
+alias gps='git push origin'
+alias gpl='git pull origin'
 alias grs='git reset'
 alias gst='git status'
 alias gsh='git stash'
