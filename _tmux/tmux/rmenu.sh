c59db78c0a030a80cf93f4370b6485f6db600b23
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-15 17:28:41 +0200

    Massive update in tmux configuration

--- _tmux/tmux/rmenu.sh
+++ _tmux/tmux/rmenu.sh
@@ -3,17 +3,22 @@
 os='unknown_OS'
 arch='unknown_arch'
 if which 'uname' >/dev/null 2>&1 ; then
-  os="$(uname -s)"
-  if [ "$(uname -s)" = 'Linux' ] ; then
-    os="$(uname -o)"
+  os=`uname -s`
+  if [ `uname -s` = 'Linux' ] ; then
+    os=`uname -o`
   fi
 
-  arch="$(uname -m)"
+  arch=`uname -m`
 fi
 
 uptime_day='unknown_uptime'
-if which 'uptime' >/dev/null 2>&1 && uptime | grep day >/dev/null 2>&1 ; then
-  uptime_day=$(uptime | awk '{print $3;}')
+if which 'uptime' >/dev/null 2>&1 ; then
+  uptime_cache=`uptime`
+  if echo "$uptime_cache" | grep day >/dev/null 2>&1 ; then
+    uptime_day=`echo $uptime_cache | awk '{print $3;}'`
+  else
+    uptime_day='< 1 day'
+  fi
 fi
 
 echo "[$os $arch] [$uptime_day]"
