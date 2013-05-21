9d696f12466b3bb56adfb0bedfb20a65b43e03ba
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-21 23:45:44 +0200

    More intelligent zsh prompt

--- _zsh/zsh/prompt.zsh
+++ _zsh/zsh/prompt.zsh
@@ -6,13 +6,16 @@ RESET_DISPLAY=$'%{\033[0m%}'
 function set_prompt()
 {
   # left prompt
-  PROMPT=''
-  PROMPT="${PROMPT}[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}]"
-  PROMPT="${PROMPT} [$RED_COLOR%20<...<%~%<<$RESET_DISPLAY] %(!.#.$) "
+  if [ -n "$(jobs)" ] ; then
+    PROMPT="[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}] "
+  else
+    PROMPT="%(0?..[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}] )"
+  fi
+  PROMPT="${PROMPT}[$RED_COLOR%25<...<%~%<<$RESET_DISPLAY] %(!.#.$) "
 
   # right prompt
   RPROMPT=''
-  if [ -n "$SHLVL" ] && [ "$SHLVL" -gt 1 ] ; then
+  if [ -n "$SHLVL" ] && [ $SHLVL -gt 1 ] ; then
     RPROMPT="{^${RED_COLOR}${SHLVL}$RESET_DISPLAY} "
   fi
   RPROMPT="${RPROMPT}[%n$RED_COLOR@$RESET_DISPLAY%M]"
