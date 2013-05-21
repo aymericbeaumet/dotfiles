f326b945c018864d46bca0a8b566c8ee2cd7516e
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-21 15:58:41 +0200

    Update zsh prompt

--- _zsh/zsh/prompt.zsh
+++ _zsh/zsh/prompt.zsh
@@ -6,10 +6,14 @@ RESET_DISPLAY=$'%{\033[0m%}'
 function set_prompt()
 {
   # left prompt
-  PROMPT="[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}]"
-  PROMPT="$PROMPT [$RED_COLOR%20<...<%~%<<$RESET_DISPLAY] %(!.#.$) "
+  PROMPT=''
+  PROMPT="${PROMPT}[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}]"
+  PROMPT="${PROMPT} [$RED_COLOR%20<...<%~%<<$RESET_DISPLAY] %(!.#.$) "
 
   # right prompt
-  RPROMPT="{^${RED_COLOR}${SHLVL}$RESET_DISPLAY}"
-  RPROMPT=" [%n$RED_COLOR@$RESET_DISPLAY%M]"
+  RPROMPT=''
+  if [ -n "$SHLVL" ] && [ "$SHLVL" -gt 1 ] ; then
+    RPROMPT="{^${RED_COLOR}${SHLVL}$RESET_DISPLAY} "
+  fi
+  RPROMPT="${RPROMPT}[%n$RED_COLOR@$RESET_DISPLAY%M]"
 }
