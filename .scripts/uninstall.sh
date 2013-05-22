9a4b8ecd8fcb669dadc6cb80158d88a3ecd372c2
Aymeric Beaumet <aymeric@beaumet.fr>
2013-05-22 09:52:45 +0200

    Simplify uninstall script (used in `make uninstall`)

--- .scripts/uninstall.sh
+++ .scripts/uninstall.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 # Fix path
-cd "$(dirname "$0")/.."
+cd "$(dirname $0)/.."
 
 # Source configuration
-source '.scripts/config'
+source .scripts/config
 
-files=(`find "$INSTALL_DIR" -mindepth 1 -maxdepth 1 -name '.*' -type l \
-  -exec echo {} \; | sort`)
+files=(`find $INSTALL_DIR -maxdepth 1 -name '.*' -type l | sort`)
 
 dotfiles_path=`pwd`
 
 for i in ${files[@]} ; do
-  if [[ `readlink "$i"` =~ "$dotfiles_path" ]] && [ -e "$i" ]; then
+  if [[ `readlink $i` =~ $dotfiles_path ]] && [ -e $i ]; then
     # If a backup file exists, restore it
-    if [ -f "$i.$BACKUP_EXT" ] ; then
-      mv -vf "$i.$BACKUP_EXT" "$i" &>/dev/null && \
+    if [ -f $i.$BACKUP_EXT ] ; then
+      mv -vf $i.$BACKUP_EXT $i &>/dev/null && \
       echo "Restoring \"$i.$BACKUP_EXT\""
     # Else just remove
     else
-      rm -vf "$i" &>/dev/null && \
+      rm -vf $i &>/dev/null && \
       echo "Removing \"$i\""
     fi
   fi
