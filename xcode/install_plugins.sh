133d3ce2d9f4cfac8b7978584c8cf0595965ce2e
Aymeric Beaumet <aymeric@beaumet.fr>
2013-06-04 10:32:43 +0200

    Add sublime text configuration files

--- xcode/install_plugins.sh
+++ xcode/install_plugins.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-#config
-PLUGINS_DIR='./plugins'
+# Config
+PLUGINS_DIR=./plugins
 
-# stop execution if not on mac os x
+# Stop execution if not on mac os x
 if [ "$(uname -s)" != 'Darwin' ] ; then
   echo 'This script has to be used on Mac OSX'
   exit 1
 fi
 
-# execute all the plugins
+# Execute all the plugins
 if ! find "$PLUGINS_DIR" -name '*.xcodeproj' -mindepth 2 -maxdepth 2 -exec open {} \; ; then
-  exit 3
+  exit 2
 fi
 
 exit 0
--- xcode/install_themes.sh
+++ xcode/install_themes.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#config
-THEMES_DIR='./themes'
-SYSTEM_THEMES_DIR="/Users/$USER/Library/Developer/Xcode/UserData/FontAndColorThemes"
-
-# stop execution if not on mac os x
+# Stop execution if not on mac os x
 if [ "$(uname -s)" != 'Darwin' ] ; then
   echo 'This script has to be used on Mac OSX'
   exit 1
 fi
+
+# Config
+THEMES_DIR=./themes
+SYSTEM_THEMES_DIR="/Users/$USER/Library/Developer/Xcode/UserData/FontAndColorThemes"
 
 if [ -z "$USER" ] ; then
   echo "Error: this script needs a valid \$USER variable"
@@ -19,7 +19,7 @@ if ! mkdir -p "$SYSTEM_THEMES_DIR" ; then
   exit 3
 fi
 
-if ! find "$THEMES_DIR" -name '*.dvtcolortheme' -mindepth 1 -maxdepth 1 -exec cp -v {} "$SYSTEM_THEMES_DIR/" \; ; then
+if ! find "$THEMES_DIR" -mindepth 1 -maxdepth 1 -name '*.dvtcolortheme' -exec ln -vsf "$(pwd)/{}" "$SYSTEM_THEMES_DIR/" \; ; then
   exit 4
 fi
 
