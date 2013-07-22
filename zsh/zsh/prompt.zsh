9b19860c774025e7862c18db251f2ccd4046634b
Aymeric Beaumet <aymeric@beaumet.me>
2013-07-22 16:36:26 +0200

    Fix prompt colors

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,6 +1,18 @@
 # Prompt
 ###
 
+autoload -U colors && colors
+
+# Preload colors specific colors
+COLOR_RESET=$'%{\033[0m%}'
+COLOR_BLUE=$'%{\033[38;05;75m%}'
+COLOR_GREEN=$'%{\033[38;05;76m%}'
+COLOR_LIGHT_GREEN=$'%{\033[38;05;46m%}'
+COLOR_YELLOW=$'%{\033[38;05;220m%}'
+COLOR_RED=$'%{\033[38;05;160m%}'
+COLOR_ORANGE=$'%{\033[38;05;202m%}'
+COLOR_VIOLET=$'%{\033[38;05;125m%}'
+
 # Git prompt
 if [ -r ~/.zsh/bundle/zsh-git-prompt/zshrc.sh ] ; then
   source ~/.zsh/bundle/zsh-git-prompt/zshrc.sh
@@ -12,33 +24,36 @@ if [ -r ~/.zsh/bundle/zsh-git-prompt/zshrc.sh ] ; then
   ZSH_THEME_GIT_PROMPT_PREFIX="("
   ZSH_THEME_GIT_PROMPT_SUFFIX=")"
   ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-  ZSH_THEME_GIT_PROMPT_BRANCH="${fg[yellow]}"
-  ZSH_THEME_GIT_PROMPT_STAGED="${fg[green]}±"
-  ZSH_THEME_GIT_PROMPT_CONFLICTS="${fg[orange]}☠"
-  ZSH_THEME_GIT_PROMPT_CHANGED="${fg[red]}≠"
+  ZSH_THEME_GIT_PROMPT_BRANCH="${COLOR_YELLOW}"
+  ZSH_THEME_GIT_PROMPT_STAGED="${COLOR_GREEN}±"
+  ZSH_THEME_GIT_PROMPT_CONFLICTS="${COLOR_ORANGE}×"
+  ZSH_THEME_GIT_PROMPT_CHANGED="${COLOR_RED}≠"
   ZSH_THEME_GIT_PROMPT_REMOTE=""
   ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
-  ZSH_THEME_GIT_PROMPT_CLEAN="${fg[green]}✓"
+  ZSH_THEME_GIT_PROMPT_CLEAN="${COLOR_LIGHT_GREEN}✓"
 fi
 
 # set content
 function set_prompt()
 {
-  # left prompt
-  if [ -n "$(jobs)" ] ; then
-    PROMPT="[${fg_bold[green]}%j${reset_color}&:?${fg_bold[green]}%?${reset_color}] "
-  else
-    PROMPT="%(0?..[${fg_bold[green]}%j${reset_color}&:?${fg_bold[green]}%?${reset_color}] )"
-  fi
+  # get git prompt
   git_prompt="$(git_super_status)"
-  PROMPT="${PROMPT}[${fg[cyan]}%25<...<%~%<<$reset_color]${git_prompt:+ $git_prompt} %(!.#.$) "
+
+  # left prompt
+  PROMPT=''
+  if [ -n "$(jobs)" ] ; then
+    PROMPT="[$COLOR_BLUE%j$COLOR_RESET&:?$COLOR_BLUE%?$COLOR_RESET] "
+  else
+    PROMPT="%(0?..[$COLOR_BLUE%j$COLOR_RESET&:?$COLOR_BLUE%?$COLOR_RESET] )"
+  fi
+  PROMPT="${PROMPT}[$COLOR_BLUE%25<...<%~%<<$COLOR_RESET]${git_prompt:+ $git_prompt} %(!.#.$) "
 
   # right prompt
   RPROMPT=''
   if [ -n "$SHLVL" ] && [ $SHLVL -gt 1 ] ; then
-    RPROMPT="{^${fg[cyan]}${SHLVL}$reset_color}"
+    RPROMPT="${RPROMPT:+$RPROMPT }{^$COLOR_BLUE$SHLVL$COLOR_RESET}"
   fi
   if [ -n "$SSH_CLIENT" ] ; then
-    RPROMPT="${RPROMPT:+$RPROMPT }[%n${fg[cyan]}@$reset_color%M]"
+    RPROMPT="${RPROMPT:+$RPROMPT }[%n$COLOR_BLUE@$COLOR_RESET%M]"
   fi
 }
