59a34d1a0a9e764a61ddb8fd0efc067dd0b01c14
Aymeric Beaumet <aymeric@beaumet.me>
2013-07-22 12:19:01 +0200

    Update prompt

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,27 +1,49 @@
 # Prompt
 ###
 
-# colors
-RED_COLOR=$'%{\033[31m%}'
-RESET_DISPLAY=$'%{\033[0m%}'
+# Load colors
+autoload -U colors
+colors
 
-# content
+# Git prompt
+if [ -r ~/.zsh/bundle/zsh-git-prompt/zshrc.sh ] ; then
+  source ~/.zsh/bundle/zsh-git-prompt/zshrc.sh
+
+  # Set install directory
+  export __GIT_PROMPT_DIR=~/.zsh/bundle/zsh-git-prompt
+
+  # Change defaults values
+  ZSH_THEME_GIT_PROMPT_PREFIX="("
+  ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+  ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
+  ZSH_THEME_GIT_PROMPT_BRANCH="$fg[yellow]"
+  ZSH_THEME_GIT_PROMPT_STAGED="$fg[green]±"
+  ZSH_THEME_GIT_PROMPT_CONFLICTS="$fg[orange]☠"
+  ZSH_THEME_GIT_PROMPT_CHANGED="$fg[red]≠"
+  ZSH_THEME_GIT_PROMPT_REMOTE=""
+  ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
+  ZSH_THEME_GIT_PROMPT_CLEAN="$fg[green]✓"
+fi
+
+# set content
 function set_prompt()
 {
   # left prompt
   if [ -n "$(jobs)" ] ; then
-    PROMPT="[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}] "
+    PROMPT="[${fg_bold[cyan]}%j${reset_color}&:?${fg_bold[cyan]}%?${reset_color}] "
   else
-    PROMPT="%(0?..[${RED_COLOR}%j${RESET_DISPLAY}&:?${RED_COLOR}%?${RESET_DISPLAY}] )"
+    PROMPT="%(0?..[${fg_bold[cyan]}%j${reset_color}&:?${fg_bold[cyan]}%?${reset_color}] )"
   fi
-  PROMPT="${PROMPT}[$RED_COLOR%25<...<%~%<<$RESET_DISPLAY] %(!.#.$) "
+  git_prompt="$(git_super_status)"
+  PROMPT="${PROMPT}[${fg[cyan]}%25<...<%~%<<$reset_color]${git_prompt:+ $git_prompt} %(!.#.$) "
 
   # right prompt
   RPROMPT=''
   if [ -n "$SHLVL" ] && [ $SHLVL -gt 1 ] ; then
-    RPROMPT="{^${RED_COLOR}${SHLVL}$RESET_DISPLAY}"
+    RPROMPT="{^${fg[cyan]}${SHLVL}$reset_color}"
   fi
   if [ -n "$SSH_CLIENT" ] ; then
-    RPROMPT="${RPROMPT:+$RPROMPT }[%n$RED_COLOR@$RESET_DISPLAY%M]"
+    RPROMPT="${RPROMPT:+$RPROMPT }[%n${fg[cyan]}@$reset_color%M]"
   fi
+  RPROMPT=''
 }
