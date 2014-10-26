79ff9c0bfac21f415fa2421add4cf0c1dd722dfd
Aymeric Beaumet <aymeric@beaumet.me>
2014-10-26 21:37:10 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,8 +53,9 @@ npm=(
 
 # Fetch dotfiles
 git clone git://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
-source "$HOME/.homesick/repos/dotfiles/home/.zlogin"
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone git@github.com:aymericbeaumet/dotfiles
+source "$HOME/.homesick/repos/dotfiles/home/.zlogin"
 homeshick link dotfiles
 
 # Install Brew
