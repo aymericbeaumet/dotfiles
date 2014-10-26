1d1a9c041a18b9b0b57e807989001d1b04c77f39
Aymeric Beaumet <aymeric@beaumet.me>
2014-10-26 21:57:54 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -51,13 +51,6 @@ npm=(
   watchify
 )
 
-# Fetch dotfiles
-git clone git://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick clone git@github.com:aymericbeaumet/dotfiles
-source "$HOME/.homesick/repos/dotfiles/home/.zlogin"
-homeshick link dotfiles
-
 # Install Brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
