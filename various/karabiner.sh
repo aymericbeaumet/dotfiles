89476c5f90ead362d491b5fcdc02294436a3a4b0
Aymeric Beaumet <aymeric@beaumet.me>
2015-05-26 00:34:45 +0200

    feat(karabiner): update

--- various/karabiner.sh
+++ various/karabiner.sh
@@ -2,240 +2,236 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.control_d 1
+$cli set remap.hyper_j 1
 /bin/echo -n .
-$cli set remap.hyper_b 1
+$cli set remap.control_i 1
 /bin/echo -n .
-$cli set remap.shifts_to_parenthesis 1
+$cli set remap.modifiers.space_to_hyper_space 1
 /bin/echo -n .
-$cli set remap.emacs.control_d 1
+$cli set remap.control_b 1
 /bin/echo -n .
-$cli set remap.emacs.control_y 1
+$cli set remap.control_y 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.y_to_j 1
+$cli set remap.pc_application_to_control_escape 1
 /bin/echo -n .
-$cli set remap.options_to_controls 1
+$cli set remap.hyper_s 1
 /bin/echo -n .
-$cli set remap.hyper_m 1
+$cli set remap.modifiers.tab_backslash_to_hyper_tab_backslash 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.i_to_u 1
+$cli set remap.qwerty_to_colemak.r_to_p (+ ctrl) 1
+/bin/echo -n .
+$cli set remap.hyper_i 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.u_to_l 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.o_to_y 1
+/bin/echo -n .
+$cli set repeat.consumer_initial_wait 200
 /bin/echo -n .
 $cli set remap.emacs.alt_d 1
 /bin/echo -n .
-$cli set remap.modifiers.commands_to_commands_curly_braces 1
+$cli set remap.qwerty_to_colemak.i_to_u 1
+/bin/echo -n .
+$cli set remap.colemak.tilde 1
+/bin/echo -n .
+$cli set remap.control_k 1
+/bin/echo -n .
+$cli set remap.hyper_r 1
+/bin/echo -n .
+$cli set remap.hyper_/ 1
+/bin/echo -n .
+$cli set remap.colemak.space_to_altgr 1
+/bin/echo -n .
+$cli set remap.control_d 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.r_to_p 1
+/bin/echo -n .
+$cli set remap.options_to_controls 1
+/bin/echo -n .
+$cli set remap.emacs.control_p 1
+/bin/echo -n .
+$cli set remap.alt_f 1
+/bin/echo -n .
+$cli set remap.emacs.alt_greater_than 1
+/bin/echo -n .
+$cli set remap.emacs.control_b 1
+/bin/echo -n .
+$cli set remap.emacs.control_w 1
+/bin/echo -n .
+$cli set remap.hyper_h 1
+/bin/echo -n .
+$cli set remap.shifts_to_shifts_parenthesis 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.b_to_b 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.h_to_h 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.y_to_j 1
+/bin/echo -n .
+$cli set remap.colemak.diaresis 1
+/bin/echo -n .
+$cli set repeat.initial_wait 999999
+/bin/echo -n .
+$cli set remap.emacs.control_i 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.n_to_k 1
+/bin/echo -n .
+$cli set remap.pc_application_control_escape 1
+/bin/echo -n .
+$cli set remap.modifiers.shifts_to_shifts_parenthesis 1
+/bin/echo -n .
+$cli set remap.alt_b 1
+/bin/echo -n .
+$cli set remap.modifiers.command_command_to_hyper 1
+/bin/echo -n .
+$cli set remap.commands_to_curly_braces 1
+/bin/echo -n .
+$cli set remap.control_m 1
+/bin/echo -n .
+$cli set remap.colemak.acute 1
 /bin/echo -n .
 $cli set remap.control_f 1
 /bin/echo -n .
+$cli set remap.emacs.control_a 1
+/bin/echo -n .
+$cli set remap.emacs.control_h 1
+/bin/echo -n .
+$cli set remap.modifiers.commands_to_commands_curly_braces 1
+/bin/echo -n .
+$cli set remap.emacs.control_v 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.l_to_i 1
+/bin/echo -n .
+$cli set remap.hyper_p 1
+/bin/echo -n .
 $cli set remap.modifiers.return_to_control_return 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.s_to_r 1
+$cli set remap.modifiers.options_to_options_brackets 1
 /bin/echo -n .
-$cli set remap.control_to_option 1
+$cli set remap.hyper_f 1
 /bin/echo -n .
-$cli set remap.alt_d 1
+$cli set general.dont_remap_external 1
 /bin/echo -n .
-$cli set remap.colemak.left_control_left_arrow_to_alt_gr 1
+$cli set remap.control_h 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.a_to_a 1
+$cli set remap.qwerty_to_colemak.f_to_t 1
 /bin/echo -n .
-$cli set remap.hyper_j 1
+$cli set remap.options_to_control_brackets 1
 /bin/echo -n .
-$cli set remap.emacs.alt_less_than 1
+$cli set remap.colemak.circumflex 1
+/bin/echo -n .
+$cli set remap.emacs.control_u 1
+/bin/echo -n .
+$cli set remap.control_a 1
+/bin/echo -n .
+$cli set remap.colemak.grave 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.d_to_s 1
+/bin/echo -n .
+$cli set remap.qwerty2colemak 1
+/bin/echo -n .
+$cli set remap.emacs.control_n 1
 /bin/echo -n .
 $cli set remap.modifiers.tab_to_hyper_tab 1
 /bin/echo -n .
-$cli set repeat.wait 999999
+$cli set remap.qwerty_to_colemak.w_to_w 1
 /bin/echo -n .
-$cli set remap.controls_to_brackets 1
+$cli set remap.emacs.alt_v 1
+/bin/echo -n .
+$cli set remap.hyper_n 1
+/bin/echo -n .
+$cli set remap.return_to_control_return 1
+/bin/echo -n .
+$cli set remap.emacs.control_f 1
+/bin/echo -n .
+$cli set remap.hyper_d 1
+/bin/echo -n .
+$cli set remap.emacs.alt_f 1
+/bin/echo -n .
+$cli set remap.hyper_w 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.semicolon_to_o 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.s_to_r 1
+/bin/echo -n .
+$cli set remap.emacs.alt_b 1
+/bin/echo -n .
+$cli set remap.emacs.control_m 1
+/bin/echo -n .
+$cli set remap.control_to_option 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.p_to_semicolon 1
+/bin/echo -n .
+$cli set remap.hyper_m 1
+/bin/echo -n .
+$cli set remap.colemak.punctuation_and_symbols 1
+/bin/echo -n .
+$cli set remap.emacs.alt_less_than 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.m_to_m 1
+/bin/echo -n .
+$cli set remap.hyper 1
+/bin/echo -n .
+$cli set remap.hyper_c 1
+/bin/echo -n .
+$cli set remap.alt_d 1
+/bin/echo -n .
+$cli set remap.hyper_v 1
+/bin/echo -n .
+$cli set remap.commands_to_commands_curly_braces 1
 /bin/echo -n .
 $cli set remap.qwerty_to_colemak.e_to_f 1
 /bin/echo -n .
 $cli set remap.emacs.control_e 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.w_to_w 1
-/bin/echo -n .
-$cli set remap.control_h 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.t_to_g 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.semicolon_to_o 1
-/bin/echo -n .
-$cli set remap.hyper_r 1
-/bin/echo -n .
-$cli set remap.emacs.control_u 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.k_to_e 1
-/bin/echo -n .
-$cli set remap.hyper_d 1
-/bin/echo -n .
-$cli set remap.hyper_/ 1
-/bin/echo -n .
-$cli set remap.doublepresscommandQ 1
-/bin/echo -n .
-$cli set remap.pc_application_to_control_escape 1
-/bin/echo -n .
-$cli set remap.emacs.control_p 1
-/bin/echo -n .
-$cli set remap.colemak.tilde 1
-/bin/echo -n .
-$cli set remap.pc_application_control_escape 1
-/bin/echo -n .
-$cli set remap.colemak.circumflex 1
-/bin/echo -n .
-$cli set remap.qwerty2colemak 1
-/bin/echo -n .
-$cli set remap.colemak.punctuation_and_symbols 1
-/bin/echo -n .
-$cli set remap.hyper 1
-/bin/echo -n .
-$cli set remap.control_a 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.u_to_l 1
-/bin/echo -n .
-$cli set remap.emacs.control_k 1
-/bin/echo -n .
-$cli set remap.hyper_a 1
-/bin/echo -n .
-$cli set remap.emacs.control_f 1
-/bin/echo -n .
-$cli set remap.commands_to_commands_curly_braces 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.r_to_p (+ ctrl) 1
-/bin/echo -n .
 $cli set remap.options_to_options_brackets 1
-/bin/echo -n .
-$cli set remap.hyper_l 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.j_to_n 1
-/bin/echo -n .
-$cli set remap.alt_f 1
-/bin/echo -n .
-$cli set repeat.initial_wait 999999
-/bin/echo -n .
-$cli set remap.emacs.control_a 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.b_to_b 1
-/bin/echo -n .
-$cli set remap.emacs.control_v 1
-/bin/echo -n .
-$cli set remap.hyper_w 1
-/bin/echo -n .
-$cli set remap.colemak.space_to_altgr 1
-/bin/echo -n .
-$cli set remap.hyper_i 1
-/bin/echo -n .
-$cli set general.dont_remap_any_pointing 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.n_to_k 1
-/bin/echo -n .
-$cli set remap.emacs.alt_f 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.o_to_y 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.l_to_i 1
-/bin/echo -n .
-$cli set remap.control_w 1
-/bin/echo -n .
-$cli set remap.control_n 1
-/bin/echo -n .
-$cli set remap.swap_number_and_symbol 1
 /bin/echo -n .
 $cli set remap.control_e 1
 /bin/echo -n .
-$cli set remap.hyper_t 1
+$cli set remap.hyper_l 1
 /bin/echo -n .
-$cli set remap.return_to_control_return 1
+$cli set remap.qwerty_to_colemak.t_to_g 1
 /bin/echo -n .
-$cli set remap.hyper_f 1
-/bin/echo -n .
-$cli set remap.colemak.diaresis 1
-/bin/echo -n .
-$cli set remap.emacs.alt_greater_than 1
-/bin/echo -n .
-$cli set remap.shifts_to_shifts_parenthesis 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.r_to_p 1
-/bin/echo -n .
-$cli set remap.modifiers.shifts_to_shifts_parenthesis 1
-/bin/echo -n .
-$cli set remap.colemak.grave 1
-/bin/echo -n .
-$cli set remap.control_y 1
-/bin/echo -n .
-$cli set remap.emacs.control_b 1
-/bin/echo -n .
-$cli set remap.hyper_c 1
-/bin/echo -n .
-$cli set remap.emacs.control_w 1
-/bin/echo -n .
-$cli set remap.control_p 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.g_to_d 1
-/bin/echo -n .
-$cli set remap.emacs.alt_b 1
-/bin/echo -n .
-$cli set remap.hyper_n 1
-/bin/echo -n .
-$cli set remap.modifiers.space_to_hyper_space 1
-/bin/echo -n .
-$cli set general.dont_remap_external 1
-/bin/echo -n .
-$cli set remap.emacs.control_m 1
-/bin/echo -n .
-$cli set remap.emacs.alt_v 1
-/bin/echo -n .
-$cli set remap.control_i 1
-/bin/echo -n .
-$cli set remap.hyper_k 1
-/bin/echo -n .
-$cli set remap.emacs.control_h 1
-/bin/echo -n .
-$cli set remap.alt_b 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.m_to_m 1
-/bin/echo -n .
-$cli set remap.modifiers.tab_backslash_to_hyper_tab_backslash 1
-/bin/echo -n .
-$cli set remap.hyper_v 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.d_to_s 1
-/bin/echo -n .
-$cli set remap.hyper_h 1
-/bin/echo -n .
-$cli set remap.control_k 1
-/bin/echo -n .
-$cli set remap.options_to_control_brackets 1
-/bin/echo -n .
-$cli set remap.qwerty_to_colemak.p_to_semicolon 1
-/bin/echo -n .
-$cli set remap.control_b 1
-/bin/echo -n .
-$cli set remap.modifiers.command_command_to_hyper 1
-/bin/echo -n .
-$cli set remap.hyper_s 1
-/bin/echo -n .
-$cli set remap.commands_to_curly_braces 1
+$cli set remap.colemak.left_control_left_arrow_to_alt_gr 1
 /bin/echo -n .
 $cli set remap.hyper_space 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.h_to_h 1
+$cli set remap.hyper_b 1
 /bin/echo -n .
-$cli set remap.emacs.control_n 1
+$cli set remap.control_n 1
 /bin/echo -n .
-$cli set remap.qwerty_to_colemak.f_to_t 1
+$cli set remap.qwerty_to_colemak.a_to_a 1
+/bin/echo -n .
+$cli set repeat.wait 999999
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.g_to_d 1
+/bin/echo -n .
+$cli set remap.controls_to_brackets 1
+/bin/echo -n .
+$cli set remap.hyper_k 1
+/bin/echo -n .
+$cli set remap.emacs.control_d 1
+/bin/echo -n .
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set remap.emacs.control_k 1
+/bin/echo -n .
+$cli set remap.shifts_to_parenthesis 1
+/bin/echo -n .
+$cli set remap.emacs.control_y 1
+/bin/echo -n .
+$cli set remap.hyper_a 1
+/bin/echo -n .
+$cli set remap.qwerty_to_colemak.j_to_n 1
+/bin/echo -n .
+$cli set remap.control_w 1
+/bin/echo -n .
+$cli set remap.hyper_t 1
+/bin/echo -n .
+$cli set remap.control_p 1
 /bin/echo -n .
 $cli set remap.modifiers.pc_application_to_control_escape 1
 /bin/echo -n .
-$cli set repeat.consumer_initial_wait 200
-/bin/echo -n .
-$cli set remap.modifiers.options_to_options_brackets 1
-/bin/echo -n .
-$cli set remap.emacs.control_i 1
-/bin/echo -n .
-$cli set remap.control_m 1
-/bin/echo -n .
-$cli set remap.hyper_p 1
-/bin/echo -n .
-$cli set remap.colemak.acute 1
+$cli set remap.qwerty_to_colemak.k_to_e 1
 /bin/echo -n .
 /bin/echo
