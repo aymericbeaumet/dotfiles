10a045ab6647997328f68ac7b65633e33dc0d25b
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-02-05 07:40:04 +0100

    update

--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 8,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      experimentalObjectRestSpread: true,
-    }
-  },
-  rules: {
-    'comma-dangle': ['error', 'always'],
-    'semi': ['error', 'never'],
-  },
-}
+module.exports = {}
