bfa81b746e69cd7faea88b2bbc50071a089d3a5d
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-02-11 01:21:54 +0100

    update

---
+++ shallow/.hammerspoon/safari.lua
@@ -0,0 +1,15 @@
+local module = {}
+
+function module.toggleBookmarks()
+  print("todo")
+end
+
+function module.refreshWithoutCache()
+  print("todo")
+end
+
+function module.closeOtherTabs()
+  print("todo")
+end
+
+return module
